777979d2f7651bc315698c5f9aa5d03d6866c09f
iolm6980 <133768355+iolm6980@users.noreply.github.com>
2024-03-25 23:23:21 +0900

    테스트3

--- src/test/java/com/match/matching/matchingTest/match.java
+++ src/test/java/com/match/matching/matchingTest/match.java
@@ -17,75 +17,58 @@ import java.util.stream.IntStream;
 public class match {
     @Autowired
     private ChatService chatService;
-
-    @Test
-    public void test(){
-        Player player = Player.builder()
-                .game(Game.LOL)
-                .rank(Rank.RANK)
-                .gameType(GameType.TFT)
-                .tier(Tier.GOLD)
-                .line(Line.TOP)
-                .lineList(3)
-                .build();
-        String roomId = chatService.enterPlayer(player);
-        System.out.println("roomId : " + roomId);
-        System.out.println(chatService.findRoomById(roomId).getRoomId());
-
-    }
-
     @Test
     public void player(){
-        LOL lol = new LOL();
-        Player player = Player.builder()
-                .game(Game.LOL)
-                .rank(Rank.RANK)
-                .gameType(GameType.TEAM)
-                .tier(Tier.GOLD)
-                .line(Line.TOP)
-                .lineList(31)
-                .build();
-        ChatRoom chat = ChatRoom.builder()
-                .game(Game.LOL)
-                .gameType(GameType.TEAM)
-                .rank(Rank.RANK)
-                .tier(Tier.GOLD)
-                .line(Line.MID)
-                .lineList(31)
-                .build();
-
-        ChatRoom chat1 = ChatRoom.builder()
-                .game(Game.LOL)
-                .gameType(GameType.TEAM)
-                .tier(Tier.GOLD)
-                .rank(Rank.RANK)
-                .line(Line.MID)
-                .lineList(31)
-                .build();
-        lol.add(chat);
-        lol.add(chat1);
-
-        System.out.println(player.getGameType() + " / " + player.getTier() + " / " + player.getLine());
-        IntStream.rangeClosed(1,300).forEach(
-                i ->{
-                    ChatRoom chatRoom = ChatRoom.builder()
-                            .roomId(UUID.randomUUID().toString())
-                            .game(Game.LOL)
-                            .rank(Rank.getRandom())
-                            .gameType(GameType.getRandom())
-                            .tier(Tier.getRandom())
-                            .line(Line.getRandom())
-                            .lineList((int) (Math.random() * 31))
-                            .build();
-                    lol.add(chatRoom);
-                }
-        );
-        List<ChatRoom> list = lol.getFilteringRoom(player);
-        System.out.println("-------------------------------------------------------------------------");
-        lol.show();
-        System.out.println("-------------------------------------------------------------------------");
-        list.forEach(chatRoom -> System.out.println( chatRoom.getGameType() + " / " + chatRoom.getTier() + " / " + chatRoom.getLine() + " / "
-                + Integer.toBinaryString(chatRoom.getLineList())));
+//        LOL lol = new LOL();
+//        Player player = Player.builder()
+//                .game(Game.LOL)
+//                .rank(Rank.RANK)
+//                .gameType(GameType.TEAM)
+//                .tier(Tier.GOLD)
+//                .line(Line.TOP)
+//                .lineList(31)
+//                .build();
+//        ChatRoom chat = ChatRoom.builder()
+//                .game(Game.LOL)
+//                .gameType(GameType.TEAM)
+//                .rank(Rank.RANK)
+//                .tier(Tier.GOLD)
+//                .line(Line.MID)
+//                .lineList(31)
+//                .build();
+//
+//        ChatRoom chat1 = ChatRoom.builder()
+//                .game(Game.LOL)
+//                .gameType(GameType.TEAM)
+//                .tier(Tier.GOLD)
+//                .rank(Rank.RANK)
+//                .line(Line.MID)
+//                .lineList(31)
+//                .build();
+//        lol.add(chat);
+//        lol.add(chat1);
+//
+//        System.out.println(player.getGameType() + " / " + player.getTier() + " / " + player.getLine());
+//        IntStream.rangeClosed(1,300).forEach(
+//                i ->{
+//                    ChatRoom chatRoom = ChatRoom.builder()
+//                            .roomId(UUID.randomUUID().toString())
+//                            .game(Game.LOL)
+//                            .rank(Rank.getRandom())
+//                            .gameType(GameType.getRandom())
+//                            .tier(Tier.getRandom())
+//                            .line(Line.getRandom())
+//                            .lineList((int) (Math.random() * 31))
+//                            .build();
+//                    lol.add(chatRoom);
+//                }
+//        );
+//        List<ChatRoom> list = lol.getFilteringRoom(player);
+//        System.out.println("-------------------------------------------------------------------------");
+//        lol.show();
+//        System.out.println("-------------------------------------------------------------------------");
+//        list.forEach(chatRoom -> System.out.println( chatRoom.getGameType() + " / " + chatRoom.getTier() + " / " + chatRoom.getLine() + " / "
+//                + Integer.toBinaryString(chatRoom.getLineList())));
     }
 
 
