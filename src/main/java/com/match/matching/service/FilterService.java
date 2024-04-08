3ad379420ce1d70bb44966c73600000348fb1406
iolm6980 <133768355+iolm6980@users.noreply.github.com>
2024-04-08 13:01:21 +0900

    욕설 필터링 구현 테스트

--- src/main/java/com/match/matching/service/FilterService.java
+++ src/main/java/com/match/matching/service/FilterService.java
@@ -18,17 +18,24 @@ import java.util.regex.Pattern;
 @Service
 public class FilterService {
     private final String ROOT_PATH = File.separator + "var" + File.separator + "jenkins_home"+ File.separator +"workspace"+ File.separator +"match"+ File.separator;
-    private String PROFANITY_PATTERN;
+    private String PROFANITY_PATTERN = "";
 
     @PostConstruct
     public void init() throws IOException {
-        try{
-            PROFANITY_PATTERN =  Files.readString(Paths.get(ROOT_PATH + "Profanity.txt")).replaceAll("\\\"", "");
-        } catch (BeanCreationException e){
-            Path path = Paths.get(ROOT_PATH + "test.txt");
-            Files.write(path, "123".getBytes());
-            System.out.println(e);
+        File currentFolder = new File(".");
+        File[] fileList = currentFolder.listFiles();
+
+        // 파일 목록 출력
+        for (File file : fileList) {
+            System.out.println(file.getName());
         }
+//        try{
+//            PROFANITY_PATTERN =  Files.readString(Paths.get(ROOT_PATH + "Profanity.txt")).replaceAll("\\\"", "");
+//        } catch (BeanCreationException e){
+//            Path path = Paths.get(ROOT_PATH + "test.txt");
+//            Files.write(path, "123".getBytes());
+//            System.out.println(e);
+//        }
     }
     public String filterProfanity(JsonNode jsonNode){
         String message = jsonNode.get("message").asText();
